607d5d709604ba17c52c7c4338d509148e25219e
aribakidwai <108187500+aribakidwai@users.noreply.github.com>
2022-08-11 21:40:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+//var face;
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  if(keyIsDown(RIGHT_ARROW)){
+    background("red");
+  }
+//code to change background colour  to purple!!
+  if(keyDown("space")){
+    background("purple");
+  }
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+ 
+}
+
